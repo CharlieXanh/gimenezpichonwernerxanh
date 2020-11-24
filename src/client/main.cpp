893b3cbbebcfef229ea83f6e39074c8956601099
lg <gimenezleo4@outlook.fr>
2020-11-24 15:25:44 +0100

    [render] modif main pour preparer le render

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -11,10 +11,10 @@ void testSFML() {
 
 #include <state.h>
 #include <string.h>
-
+#include <render.h>
 using namespace std;
 using namespace state;
-
+using namespace render;
 int main(int argc,char* argv[])
 {
     //Exemple exemple;
@@ -25,7 +25,29 @@ int main(int argc,char* argv[])
 
 	if(argc >= 2 && strcmp(argv[1],"State") == 0 ){
 		cout << "State" << endl;
-		
+
 	}
+  if(argc >= 2 && strcmp(argv[1],"Render") == 0 )
+  {
+    sf::Window fenetre(sf::VideoMode(800, 600), "My window");
+    TilePlayer image(100.f,100.f);
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+            else
+            {
+            //fenetre.clear();
+
+            }
+        }
+    }
+  }
     return 0;
 }
