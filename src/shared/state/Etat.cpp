c8fd9f3e72328f6daf3772bbc75ed330a100e5e1
LuWe <luc.werner@ensea.fr>
2020-11-27 18:37:46 +0100

    [Etat] cpp

--- src/shared/state/Etat.cpp
+++ src/shared/state/Etat.cpp
@@ -2,15 +2,18 @@
 
 using namespace state;
 
-// Etat::Etat (){
-//
-// }
+ // Etat::Etat (){
+ // }
 
 void Etat::init(){
 
 }
 
 int  Etat::initJoueur(Joueur joueur){
+  // joueur.e->caracteristiques->setAttaque(20);
+  // joueur->entite->position->placer(0,0);
+
+  this->joueurs.push_back(joueur);
 return 0;
 }
 
