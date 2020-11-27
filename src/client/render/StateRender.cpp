1b9194b360083cc09d8184bdc47b837ad1b9e1c5
lg <gimenezleo4@outlook.fr>
2020-11-27 17:57:09 +0100

    [render] Rendu d'affichage "fonctionnel"

--- src/client/render/StateRender.cpp
+++ src/client/render/StateRender.cpp
@@ -13,7 +13,7 @@ StateRender:: StateRender (state::Position const& position)
 	:player(position.getX(),position.getY())
 	{
 		this->map.load("res/tilemap_packed.png",sf::Vector2u(16,16));
-		this->window = new sf::RenderWindow(sf::VideoMode(500,500), "One Upon A Wei",sf::Style::Close);
+		this->window = new sf::RenderWindow(sf::VideoMode(720,720), "One Upon A Wei",sf::Style::Close);
 		this->view = new  sf::View(sf::Vector2f(0.f, 10.f), sf::Vector2f(360.f, 360.f));
 		this->menu = new LayerMenu(1);
 	}
@@ -21,12 +21,16 @@ StateRender:: StateRender (state::Position const& position)
 
 void StateRender :: updatePosition(state::Position const& position){
 	this->player.update_pos(position);
-	this->menu->updatePosition(sf::Vector2f(position.x,position.y));
+	this->menu->updatePosition(sf::Vector2f(position.getX(),position.getY()));
 }
 
 void StateRender :: update(){
 	this->window->clear();
+	view->setCenter(player.getPosition().x,player.getPosition().y);
+	window->setView(*view);
+
 	this->window->draw(map);
+	this->window->draw(*menu);
 	this->player.render_tile(*window);
 	this->window->display();
 }
