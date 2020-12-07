5cd05d19d10d3755c41a41dbf5f3cc18dd4b1565
LuWe <luc.werner@ensea.fr>
2020-12-07 17:59:28 +0100

    [State] Correction code cpp et ajout 1

--- src/shared/state/Etat.cpp
+++ src/shared/state/Etat.cpp
@@ -1,26 +1,3 @@
 #include "Etat.h"
 
 using namespace state;
-
- // Etat::Etat (){
- // }
-
-void Etat::init(){
-
-}
-
-int  Etat::initJoueur(Joueur joueur){
-  // joueur.e->caracteristiques->setAttaque(20);
-  // joueur->entite->position->placer(0,0);
-
-  this->joueurs.push_back(joueur);
-return 0;
-}
-
-int Etat::changeJouant(Joueur joueurSuivant){
-return 0;
-}
-
-int Etat::transitionEtat(Etat etat){
-return 0;
-}
--- src/shared/state/Joueur.cpp
+++ src/shared/state/Joueur.cpp
@@ -2,11 +2,10 @@
 
 using namespace state;
 
- Joueur :: Joueur (std::string nom, Entite& entite, int monnaie)
+ Joueur :: Joueur (std::string nom, int monnaie)
 {
 	this->nom = nom;
 	this->monnaie = monnaie;
-	this->entite =  entite;
  }
 
 void Joueur :: ajoueMonnaie(int monnaie){
