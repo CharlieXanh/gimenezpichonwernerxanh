628181a1016850b9ad6b77d185b0cba2322a3728
LuWe <luc.werner@ensea.fr>
2020-12-09 18:04:48 +0100

    [Engine] Update Engine (completion cpp) et main

--- src/shared/engine/DeplacerCommande.cpp
+++ src/shared/engine/DeplacerCommande.cpp
@@ -6,13 +6,47 @@ using namespace engine;
 using namespace state;
 using namespace std;
 
-DeplacerCommande::DeplacerCommande(state::Joueur &Jouant,
-  state::Position &positionCible) : Jouant(Jouant), positionCible(positionCible)
+DeplacerCommande::DeplacerCommande(state::Joueur &joueurCible,
+  state::Position &positionCible) : joueurCible(joueurCible), positionCible(positionCible)
 {
     id = DEPLACER;
 }
 
 void DeplacerCommande::execute(state::Etat &etat)
 {
-  //TODO
+  cout << "Deplacement d'un joueur" << endl;
+  bool allowed = true;
+  if (joueurCible.getStatut() == SEL)
+  {
+      // if the character has moves to do
+      cout << "Move chances left " << joueurCible.getDeplacements() << endl;
+      if (joueurCible.getDeplacements() > 0)
+      {
+          //TODO : Check deplacements autorisés
+          // for (auto &pos: joueurCible.deplacementsPossibles(etat))
+          // {
+          //     if (pos.egale(positionCible))
+          //     {
+          //         allowed = true;
+          //         break;
+          //     }
+          // }
+          if (allowed)
+          {
+              // Moving
+              joueurCible.getPosition().setX(positionCible.getX());
+              joueurCible.getPosition().setY(positionCible.getY());
+              joueurCible.setDeplacements(joueurCible.getDeplacements() - 1);
+              // TODO refresh to the base move quantity for each character if the are not death.
+
+              cout << "The character " << joueurCible.getNom() << " has been moved to [" << positionCible.getX() << ", " << positionCible.getY() << "]" << endl;
+              cout << "Decreasing by 1 moving chances, now the character has " << joueurCible.getDeplacements() << " move chances left" << endl;
+          }
+          else
+              cout << "The character " << joueurCible.getNom() << " can't move to that position" << endl;
+      }
+      else
+          cout << "Can't move anymore" << endl;
+  }
+  cout << "\n";
 }
---
+++ src/shared/engine/VictoireCommande.cpp
@@ -0,0 +1,13 @@
+#include "VictoireCommande.h"
+
+using namespace engine;
+using namespace state;
+using namespace std;
+
+VictoireCommande::VictoireCommande(){
+  id = VICTOIRE;
+}
+
+void VictoireCommande::execute (state::Etat& state){
+  //todo
+}
