6837525d14c0ca0a47cc8bd307b44fe836be4ba8
Raphaël <pichon.raphael@icloud.com>
2021-01-04 16:53:12 +0100

    [IA] Ajout computePath à Heuristic

--- src/shared/ai/HeuristicAI.cpp
+++ src/shared/ai/HeuristicAI.cpp
@@ -94,6 +94,80 @@ void HeuristicAI::run(engine::Engine &engine)
     }
 }
 
+std::vector<std::vector<int>> HeuristicAI::computePath(std::vector<int> start,std::vector<int> end,std::vector<std::vector<int>> mapp,bool print=false){
+    int i,index;
+    Noeud* currentNoeud;
+
+    endNoeud = new Noeud(end[0],end[1],NULL,NULL);
+    startNoeud = new Noeud(start[0],start[1],NULL,endNoeud);
+
+    std::vector<Noeud*> listeFerme;
+    std::vector<Noeud*> listeOuverte;
+    listeOuverte.push_back(startNoeud);
+
+    do{
+        //Look for the best noeud in listOuverte
+        i = 0;
+        index=0;
+        currentNoeud = listeOuverte[0];
+
+        for(auto x : listeOuverte){
+            if(x->getScore()["end"]<currentNoeud->getScore()["end"]){
+                currentNoeud = x;
+                index = i;
+            }
+            i++;
+        }
+
+        listeFerme.push_back(currentNoeud);
+
+        listeOuverte = currentNoeud->voisins(mapp,listeFerme,listeOuverte,endNoeud);
+        listeOuverte.erase(listeOuverte.begin()+index);
+
+    }while(listeOuverte.size()>0 && !endNoeud->inside(listeOuverte));
+
+    if(print){
+        std::cout<<"Start : "<<startNoeud->getX()<< " " << startNoeud->getY()<<std::endl;
+        std::cout<<"End : "<<endNoeud->getX()<< " " << endNoeud->getY()<<std::endl;
+        std::cout << "Path :" << std::endl;
+
+        //Print map
+        bool pass;
+        for(int  i = 0;i<mapp.size();i++){
+            for(int j=0;j<mapp[i].size();j++){
+                pass = true;
+                for(auto x : listeFerme){
+                    if(x->getX()==i && x->getY()==j){
+                        std::cout<< "\033[1;35m" << mapp[i][j] << "\033[0m" <<" ";
+                        pass = false;
+                    }
+                }
+                if(pass){
+                    if((i==startNoeud->getX() && j==startNoeud->getY())  || (i==endNoeud->getX() && j==endNoeud->getY()))
+                        std::cout<< "\033[1;35m" << mapp[i][j] << "\033[0m" <<" ";
+                    else if (mapp[i][j]==1)
+                        std::cout<< "\033[1;31m" << mapp[i][j] << "\033[0m" <<" ";
+                    else if(mapp[i][j]==0)
+                        std::cout<< "\033[1;37m" << mapp[i][j] << "\033[0m" <<" ";
+                }
+            }
+            std::cout<<std::endl;
+        }
+    }
+    if(endNoeud->inside(listeOuverte)){
+        endNoeud->parent = currentNoeud;
+        Noeud* cheminNoeud = endNoeud;
+        while(cheminNoeud!=NULL){
+            std::vector<int> add = {cheminNoeud->getX(),cheminNoeud->getY()};
+            path.insert(path.begin(), add );
+            cheminNoeud = cheminNoeud->parent;
+        }
+        return path;
+    }
+    path.push_back(start);
+    return path;
+}
+
 
 int HeuristicAI::getNumeroEnnemi (){
     return numeroEnnemi;
